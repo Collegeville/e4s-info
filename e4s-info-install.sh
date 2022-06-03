9b860944ba374c22a3235dbb5da03869b458c09a
CGROVE001 <cgrove001@vplincsci052.ad.csbsju.edu>
2022-06-03 12:07:35 -0500

    Fixed Typo
    Fixed a typo in the install file. Changed manuel to manual.

--- e4s-info-install.sh
+++ e4s-info-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p /usr/local/bin/
-cp e4s-info-command/* /usr/local/bin/
+cp -r e4s-info-command/* /usr/local/bin/
 mkdir -p /usr/share/man/man1/
-cp e4s-info-manuel/e4s-info.gz /usr/share/man/man1/
+cp e4s-info-manual/e4s-info.1.gz /usr/share/man/man1/
