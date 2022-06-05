e6f89630c998d28f19c6c0170efb07d5aec647a5
Carter Grove <cgrove001@vplincsci049.ad.csbsju.edu>
2022-06-05 13:26:23 -0500

    Installation Update
    The path to the man script was updated to include local. That way, it will be more accessable to mac users and safer to use.

--- e4s-info-install.sh
+++ e4s-info-install.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /usr/local/bin/
 cp -r e4s-info-command/* /usr/local/bin/
-mkdir -p /usr/share/man/man1/
-cp e4s-info-manual/e4s-info.1.gz /usr/share/man/man1/
+mkdir -p /usr/local/share/man/man1/
+cp e4s-info-manual/e4s-info.1.gz /usr/local/share/man/man1/
